487db6e447ddd4cff503fe6586d8dddb8677f489
Santeri Liukkonen <santeri@Santeris-MacBook-Pro-2.local>
2018-10-07 19:53:47 +0200

    Added and imported Vuex, connected with main Vue library.

---
+++ api-id.md
@@ -0,0 +1,7 @@
+### Client ID:
+
+aa8a77624115af0
+
+### Client secret:
+
+7c443c47b0db5bbe2e596d8fd7a6f15095d162c2
---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+// Import Vue and Vuex
+import Vuex from "vuex";
+import Vue from "vue";
+
+// Connect Vuex with Vue
+Vue.use(Vuex);
+
+// Initiate a new store that contins getters, setters, mutations, actions...
+export default new Vuex.Store({
+    modules: {}
+});
