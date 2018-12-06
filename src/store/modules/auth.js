6ce25e17d6f97dc4c96cd5c35a30d43cd0f65b87
Santeri Liukkonen <sliukkonen@Santeris-MBP.lan>
2018-12-06 13:06:00 +0100

    Configured authentication and vue router template navigation

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,14 +1,15 @@
 import api from "../../api/imgur";
 import qs from "qs";
+import {router} from "../../main";
 
 // Initial state
 const state = {
-    token: null
+    token: window.localStorage.getItem("imgur_token")
 };
 
 // Getting boolean if logged in or not
 const getters = {
-    isLoggedIn: state => !!state.token        
+    isLoggedIn: state => !!state.token      
 };
 
 // Never call mutation directly, but rather via commit! Put complexity and most of code into actions
@@ -19,9 +20,12 @@ const actions = {
     finalizeLogin({ commit }, hash) {
         const parsed = qs.parse(hash.replace("#", ""));
         commit("setToken", parsed.access_token);
+        window.localStorage.setItem("imgur_token", parsed.access_token);
+        router.push("/");
     },
     logout: ({ commit }) => {
         commit("setToken", null);
+        window.localStorage.removeItem("imgur_token");
     }
 };
 
